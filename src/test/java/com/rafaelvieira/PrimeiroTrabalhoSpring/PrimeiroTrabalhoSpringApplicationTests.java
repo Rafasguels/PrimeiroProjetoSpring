4caed1b966cc59f14aaa07b70ae45f044c2c21c8
Rafasguels <rafael.vieira159@outlook.com>
2023-02-26 15:49:26 -0300

    Projeto criado

---
+++ src/test/java/com/rafaelvieira/PrimeiroTrabalhoSpring/PrimeiroTrabalhoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelvieira.PrimeiroTrabalhoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroTrabalhoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
