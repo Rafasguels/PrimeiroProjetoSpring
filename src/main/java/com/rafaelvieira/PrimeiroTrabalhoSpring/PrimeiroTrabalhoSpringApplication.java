8f7774d016e3a7bfa679bf4dcd9d23fa9945c888
Rafasguels <rafael.vieira159@outlook.com>
2023-02-26 18:24:10 -0300

    test

--- src/main/java/com/rafaelvieira/PrimeiroTrabalhoSpring/PrimeiroTrabalhoSpringApplication.java
+++ src/main/java/com/rafaelvieira/PrimeiroTrabalhoSpring/PrimeiroTrabalhoSpringApplication.java
@@ -3,7 +3,7 @@ package com.rafaelvieira.PrimeiroTrabalhoSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="br.com.tokenlab.controller")
 public class PrimeiroTrabalhoSpringApplication {
 
 	public static void main(String[] args) {
